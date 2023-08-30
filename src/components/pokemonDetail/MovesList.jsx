0b21ab66271b4afafeb6ae60f15d24173495e5b0
jguayanes <julianguayanes4@gmail.com>
2023-08-29 19:05:31 -0500

    change width of movements

--- src/components/pokemonDetail/MovesList.jsx
+++ src/components/pokemonDetail/MovesList.jsx
@@ -4,7 +4,7 @@ const MovesList = ({ moves }) => {
       <h2 className="font-bold text-xl">Movements</h2>
       <article className="flex flex-wrap gap-4">
         {moves?.map((move) => (
-          <ul className="bg-gray-300 rounded-md" key={move.name}>
+          <ul className="bg-gray-300 rounded-md w-[90px] text-center font-semibold" key={move.name}>
             {move.name}
           </ul>
         ))}
